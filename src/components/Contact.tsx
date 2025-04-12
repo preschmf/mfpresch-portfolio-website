14e47e7988f6c04c4b912702cc0510eba22eb4b1
preschmf <preschmf@gmail.com>
2025-04-11 23:32:04 -0400

    make app responsive and add the last links

--- src/components/Contact.tsx
+++ src/components/Contact.tsx
@@ -1,19 +1,20 @@
 import './Contact.css'
 import { Button } from 'react-bootstrap'
+const resumeGoogleDriveLink = 'https://docs.google.com/document/d/1bctKVQMthDKohA0wW6acLlDY_mhhGSWz/edit?usp=sharing&ouid=112284854934224240639&rtpof=true&sd=true'
 
 const Contact = () => {
   return (
-      <div className='contact-container' id='contact'>
-        <div className='button-container'>
-          <Button
-            variant='tertiary'
-            onClick={() => (window.location.href = 'mailto:preschmf@gmail.com')}
-          >
-            Email Me
-          </Button>
-          <Button variant='tertiary'>My Resume</Button>
-        </div>
+    <div className='contact-container' id='contact'>
+      <div className='button-container'>
+        <Button
+          variant='tertiary'
+          onClick={() => (window.location.href = 'mailto:preschmf@gmail.com')}
+        >
+          Email Me
+        </Button>
+        <Button variant='tertiary' href={resumeGoogleDriveLink} target='_blank'>My Resume</Button>
       </div>
+    </div>
   )
 }
 
--- src/components/SiteNavBar.tsx
+++ src/components/SiteNavBar.tsx
@@ -28,7 +28,6 @@ const SiteNavBar = () => {
   const onUpdateActiveSection = (section: string) => {
     setActiveSection(section)
     const element = document.getElementById(section)
-    //use below code?
     if (element) {
       element.scrollIntoView({ behavior: 'smooth' })
     }
@@ -37,62 +36,55 @@ const SiteNavBar = () => {
   return (
     <Navbar expand='lg' className={`${scrolled ? 'scrolled' : ''}`}>
       <Container>
-        <Nav className='mfpresch-nav d-flex flex-row'>
+        <Nav className='mfpresch-nav'>
           <Navbar.Brand href='#home' className='brand-section'>
-            <img
-              alt=''
-              src={mpLogo}
-              width='30'
-              height='30'
-            />{' '}
-            Michael Presch
+            <img alt='' src={mpLogo} width='30' height='30' /> Michael Presch
           </Navbar.Brand>
-          <Nav.Link
-            href='#home'
-            className={activeSection === 'home' ? 'active-section' : ''}
-            onClick={() => onUpdateActiveSection('home')}
+          <Navbar.Toggle aria-controls='responsive-navbar-nav' />
+          <Navbar.Collapse
+            className='collapse-content'
+            id='responsive-navbar-nav'
           >
-            Home
-          </Nav.Link>
-          <Nav.Link
-            href='#contact'
-            className={activeSection === 'contact' ? 'active-section' : ''}
-            onClick={() => onUpdateActiveSection('contact')}
-          >
-            Contact
-          </Nav.Link>
-          <Nav.Link
-            href='#projects'
-            className={activeSection === 'projects' ? 'active-section' : ''}
-            onClick={() => onUpdateActiveSection('projects')}
-          >
-            Projects
-          </Nav.Link>
-          <Nav.Link
-            href='#skills'
-            className={activeSection === 'skills' ? 'active-section' : ''}
-            onClick={() => onUpdateActiveSection('skills')}
-          >
-            Skills
-          </Nav.Link>
-          <span>
-            <div className='socials-icons'>
-              <a
-                href='https://github.com/preschmf'
-                target='_blank'
-                rel='noopener noreferrer'
-              >
-                <img src={githubLogo} alt='github' />
-              </a>
-              <a
-                href='https://linkedin.com/in/michael-presch-96832b35b'
-                target='_blank'
-                rel='noopener noreferrer'
-              >
-                <img src={linkedinLogo} alt='linkedin' />
-              </a>
-            </div>
-          </span>
+            <Nav.Link
+              href='#contact'
+              className={activeSection === 'contact' ? 'active-section' : ''}
+              onClick={() => onUpdateActiveSection('contact')}
+            >
+              Contact
+            </Nav.Link>
+            <Nav.Link
+              href='#projects'
+              className={activeSection === 'projects' ? 'active-section' : ''}
+              onClick={() => onUpdateActiveSection('projects')}
+            >
+              Projects
+            </Nav.Link>
+            <Nav.Link
+              href='#skills'
+              className={activeSection === 'skills' ? 'active-section' : ''}
+              onClick={() => onUpdateActiveSection('skills')}
+            >
+              Skills
+            </Nav.Link>
+            <span>
+              <div className='socials-icons'>
+                <a
+                  href='https://github.com/preschmf'
+                  target='_blank'
+                  rel='noopener noreferrer'
+                >
+                  <img src={githubLogo} alt='github' />
+                </a>
+                <a
+                  href='https://linkedin.com/in/michael-presch-96832b35b'
+                  target='_blank'
+                  rel='noopener noreferrer'
+                >
+                  <img src={linkedinLogo} alt='linkedin' />
+                </a>
+              </div>
+            </span>
+          </Navbar.Collapse>
         </Nav>
       </Container>
     </Navbar>
