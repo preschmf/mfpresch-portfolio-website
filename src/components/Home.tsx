a9408b75be8c8de6b0b865a0dd4620aefc2f02e9
preschmf <44412371+preschmf@users.noreply.github.com>
2025-05-09 17:59:59 -0400

    Update Home.tsx

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -14,7 +14,7 @@ const Home = () => {
           <p>
             &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; I have over 7 years
             of experience as a software engineer in both modern front-end and
-            back-end technologies, primarily utilizing JavaScript, C#, relational DBs, SQL DBs
+            back-end technologies, primarily utilizing JavaScript, C#, NoSQL DBs, SQL DBs
             and AWS. I look to produce high-quality, reusable code. My expertise
             extends to maintaining robust CI/CD pipelines using tools like
             Jenkins and GitHub Actions, as well as implementing automated
